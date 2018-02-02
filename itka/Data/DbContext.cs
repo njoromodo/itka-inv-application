382c08d0489766e53157d816053014186443208d
Brigite <brigite.peposhi@gmail.com>
2018-02-02 19:34:10 +0100

    Test

---
+++ itka/Data/DbContext.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.Entity;
+using itka.Models;
+using itka.Migrations;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+
+
+namespace itka.Data
+{
+    public class DbContext: IdentityDbContext<CustomUser>
+    {
+        public DbContext() : base("DefaultConnection") {
+            
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<DbContext, Configuration>());
+
+        }
+        public static DbContext Create()
+        {            
+            return new DbContext();
+        }     
+        public DbSet<tblEvent> Events { get; set; }
+        public DbSet<tblClient> Clients { get; set; }
+        public DbSet<tblUnit> Units { get; set; }
+        public DbSet<tblSupplier> Suppliers { get; set; }
+        public DbSet<tblProduct> Products { get; set; }
+        public DbSet<tblEntry> Entries { get; set; }
+        public DbSet<tblExit> Exits { get; set; }
+        public DbSet<tblWarehouse> Warehouses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder
+            //  .Properties()
+            //  .Where(p => p.PropertyType == typeof(string) &&
+            //              !p.Name.Contains("Id") &&
+            //              !p.Name.Contains("Provider"))
+            //  .Configure(p => p.HasMaxLength(2000));
+        }
+    }
+}
---
+++ itka/Services/DeleteServices.cs
@@ -0,0 +1,151 @@
+﻿using itka.Data;
+using System;
+using System.Linq;
+
+namespace itka.Services
+{
+
+    public class DeleteServices
+    {
+        private readonly DbContext _db;
+        public DeleteServices() : this(new DbContext()) { }
+
+        public DeleteServices(DbContext agendaContext)
+        {
+            _db = agendaContext;
+        }
+        public bool DeleteProduct(int id)
+        {
+            using (var dbContextTransaction = _db.Database.BeginTransaction())
+            {
+                try
+                {
+                    var usr = _db.Products.Where(x => x.id == id).First();
+                    usr.status = false;
+                    _db.SaveChanges();
+                    dbContextTransaction.Commit();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    dbContextTransaction.Rollback();
+                    return false;
+                }
+            }
+        }
+        public bool DeleteUnit(int id)
+        {
+            using (var dbContextTransaction = _db.Database.BeginTransaction())
+            {
+                try
+                {
+                    var usr = _db.Units.Where(x => x.id == id).First();
+                    usr.status = false;
+                    _db.SaveChanges();
+                    dbContextTransaction.Commit();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    dbContextTransaction.Rollback();
+                    return false;
+                }
+            }
+        }
+        public bool DeleteClient(int id)
+        {
+            using (var dbContextTransaction = _db.Database.BeginTransaction())
+            {
+                try
+                {
+                    var usr = _db.Clients.Where(x => x.id == id).First();
+                    usr.status = false;
+                    _db.SaveChanges();
+                    dbContextTransaction.Commit();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    dbContextTransaction.Rollback();
+                    return false;
+                }
+            }
+        }
+        public bool DeleteSupplier(int id)
+        {
+            using (var dbContextTransaction = _db.Database.BeginTransaction())
+            {
+                try
+                {
+                    var usr = _db.Suppliers.Where(x => x.id == id).First();
+                    usr.status = false;
+                    _db.SaveChanges();
+                    dbContextTransaction.Commit();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    dbContextTransaction.Rollback();
+                    return false;
+                }
+            }
+        }
+        public bool DeleteWarehouse(int id)
+        {
+            using (var dbContextTransaction = _db.Database.BeginTransaction())
+            {
+                try
+                {
+                    var usr = _db.Warehouses.Where(x => x.id == id).First();
+                    usr.status = false;
+                    _db.SaveChanges();
+                    dbContextTransaction.Commit();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    dbContextTransaction.Rollback();
+                    return false;
+                }
+            }
+        }
+        public bool DeleteExit(int id)
+        {
+            using (var dbContextTransaction = _db.Database.BeginTransaction())
+            {
+                try
+                {
+                    var usr = _db.Exits.Where(x => x.id == id).First();
+                    usr.status = false;
+                    _db.SaveChanges();
+                    dbContextTransaction.Commit();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    dbContextTransaction.Rollback();
+                    return false;
+                }
+            }
+        }
+        public bool DeleteEntry(int id)
+        {
+            using (var dbContextTransaction = _db.Database.BeginTransaction())
+            {
+                try
+                {
+                    var usr = _db.Entries.Where(x => x.id == id).First();
+                    usr.status = false;
+                    _db.SaveChanges();
+                    dbContextTransaction.Commit();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    dbContextTransaction.Rollback();
+                    return false;
+                }
+            }
+        }
+    }
+}
